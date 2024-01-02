3313345611ebebdc346102f3358314d89412a430
Michael <mschilling@users.noreply.github.com>
2024-01-02 23:23:21 +0000

    extend functions

--- functions/index.js
+++ functions/index.js
@@ -7,13 +7,37 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
 exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
+  logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
+});
+
+exports.activities = onRequest(async (request, response) => {
+  // import firestore
+  const db = admin.firestore();
+
+  // read all activities and return them
+  try {
+    const querySnapshot = await db.collection("activities").get();
+    let activities = [];
+    querySnapshot.forEach((doc) => {
+      activities.push({
+        id: doc.id,
+        message: doc.data().message,
+        timestamp: doc.data().timestamp.toDate(),
+      });
+    });
+    response.send(activities);
+  } catch (err) {
+    response.status(500).send(err);
+  }
 });
